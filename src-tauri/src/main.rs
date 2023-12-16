2dabfb0e94a5a1795e67bf9c29d68486670552a0
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-12-16 12:09:35 -0300

    feat: add read file tree

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,13 +2,25 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod folders;
+mod file_tree;
 
 fn main() {
   tauri::Builder::default()
+  .invoke_handler(tauri::generate_handler![
+    read_file_tree
+  ])
   .setup(|_| {
     folders::create_remind_folder_if_not_exists();
     Ok(())
   })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn read_file_tree() -> file_tree::TreeNode {
+    let documents_dir = tauri::api::path::document_dir().unwrap_or_default();
+    let remind_folder_path = documents_dir.join("Remind");
+    let file_tree = file_tree::build_tree(remind_folder_path);
+    file_tree
 }
